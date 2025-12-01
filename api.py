cd7b986bd9dcf569bfb833c8808221cf7c89b939
quatrorabes <chrisrabenold@gmail.com>
2025-12-01 11:58:38 -0800

    🔧 Fix PostgreSQL date casting for NULL/empty last_contact_date

--- api.py
+++ api.py
@@ -888,7 +888,7 @@ def get_todays_board():
 
         # Date calculation differs between PostgreSQL and SQLite
         if IS_PRODUCTION:
-            date_calc = "EXTRACT(DAY FROM (CURRENT_DATE - last_contact_date::date))"
+            date_calc = "CASE WHEN last_contact_date IS NOT NULL AND last_contact_date != '' THEN EXTRACT(DAY FROM (CURRENT_DATE - last_contact_date::date)) ELSE 999 END"
         else:
             date_calc = "CAST(julianday('now') - julianday(last_contact_date) AS INTEGER)"
 
