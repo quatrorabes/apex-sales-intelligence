f12e04680db7a36a981af4cfeadcba05073ed2a0
quatrorabes <chrisrabenold@gmail.com>
2025-12-12 13:08:14 -0800

    Comment out AllContactsView route in App component
    Comment out the AllContactsView route for future reference.

--- dashboard_v1/src/App.tsx
+++ dashboard_v1/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { TodaysBoard } from './components/TodaysBoard';
 import { ContactDetailPage } from './pages/ContactDetailPage';
-import { AllContactsView } from './pages/AllContactsView'; // If you have this
+// import { AllContactsView } from './pages/AllContactsView'; // If you have this
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="/dashboard" element={<TodaysBoard />} />
-          <Route path="/contacts" element={<AllContactsView />} />
+          {/* <Route path="/contacts" element={<AllContactsView />} /> */}
           <Route path="/contacts/:id" element={<ContactDetailPage />} />
         </Routes>
       </div>
