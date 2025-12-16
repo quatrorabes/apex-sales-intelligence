020a7ac8dcb5ba38521aea299f56b1181616117a
quatrorabes <chrisrabenold@gmail.com>
2025-12-15 17:08:56 -0800

    fix: route / to LandingPage not TodaysBoard

--- dashboard_v1/src/App.tsx
+++ dashboard_v1/src/App.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
 import TodaysBoard from './components/TodaysBoard';
 import ContactsView from './components/ContactsView';
 import ContactDetail from './pages/ContactDetail';
@@ -6,14 +8,12 @@ import ContactDetail from './pages/ContactDetail';
 function App() {
   return (
     <Router>
-      <div className="min-h-screen">
-        <Routes>
-          <Route path="/" element={<Navigate to="/todays-board" replace />} />
-          <Route path="/todays-board" element={<TodaysBoard />} />
-          <Route path="/contacts" element={<ContactsView />} />
-          <Route path="/contacts/:id" element={<ContactDetail />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/todays-board" element={<TodaysBoard />} />
+        <Route path="/contacts" element={<ContactsView />} />
+        <Route path="/contacts/:id" element={<ContactDetail />} />
+      </Routes>
     </Router>
   );
 }
